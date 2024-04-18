7b0a40cb1db7503118029eda7e0587c29e115f7b
dealtime101 <dealtime101@hotmail.com>
2024-04-17 21:43:27 -0400

    JSON File
    Adding JSON file dump structures folders.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 structure = {
             'Music': ['Rock', 'Jazz', 'Pop'],
@@ -8,13 +9,25 @@ structure = {
             }
 
 
-def create_folder(folder):
-    for key, values in folder.items():
+def create_folder(base, folder_path):
+    for key, values in folder_path.items():
         for value in values:
-            folder = '{0}/{1}/{2}'.format(base, key, value)
-            os.makedirs(folder)
-            print('Folder Creation {0}'.format(folder))
+            folder_path = '{0}/{1}/{2}'.format(base, key, value)
+            if not os.path.exists(folder_path):
+                os.makedirs(folder_path)
+                print(f'Folder created: {folder_path}')
+            else:
+                print(f'Folder already exists: {folder_path}')
 
 
-base = r'C:\Users\dclairvoyant\OneDrive - Ubisoft\Documents\GitHub\FolderStructures\Structure'
-base.replace('\\', '/')
+base = os.path.join(os.environ['USERPROFILE'], 'Downloads/Structures')
+json_file = os.path.join(base, 'settings.json')
+
+
+def write_json(json_file, data):
+    with open(json_file, 'w') as f:
+        json.dump(data, f, indent=4)
+
+
+create_folder(base, structure)
+write_json(json_file, structure)
